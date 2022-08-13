de3157f8a8206d8ec75d86099ae14a272170ddbd
satotory <ruskosha@gmail.com>
2022-08-13 15:51:43 +0300

    added test with negative numbers

--- quicksort_test.go
+++ quicksort_test.go
@@ -9,3 +9,8 @@ func TestSimpleArr(t *testing.T) {
 	arr := []int{0, 11, 5, 3, 6, 7, 2, 1, 0, 99}
 	fmt.Println(quickSort(arr, 0, len(arr)-1))
 }
+
+func TestArrWithNegative(t *testing.T) {
+	arr := []int{-10, 2011, 5, 3, 6, 7, 2, 15, 0, 99, -10, -101, -5, 3, 6, 7, 2, 1, 0, -99}
+	fmt.Println(quickSort(arr, 0, len(arr)-1))
+}
