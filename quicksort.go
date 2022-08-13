f5ad995c849c55c4696240017ee4098b97149c9e
satotory <ruskosha@gmail.com>
2022-08-13 15:50:06 +0300

    simple code

--- quicksort.go
+++ quicksort.go
@@ -1,5 +1,27 @@
 package quicksort
 
-func quicksort() {
+func quickSort(arr []int, first, last int) []int {
+	if first < last {
+		var p int
+		arr, p = parts(arr, first, last)
+		arr = quickSort(arr, first, p-1)
+		arr = quickSort(arr, p+1, last)
+	}
+	return arr
+}
 
+func parts(arr []int, first, last int) ([]int, int) {
+	vlast := arr[last]
+	c := first
+
+	for i := c; i < last; i++ {
+		if arr[i] < vlast {
+			arr[c], arr[i] = arr[i], arr[c]
+			c++
+		}
+	}
+
+	arr[c], arr[last] = arr[last], arr[c]
+
+	return arr, c
 }
--- quicksort_test.go
+++ quicksort_test.go
@@ -1,7 +1,11 @@
 package quicksort
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-func TestQuicksort(t *testing.T) {
-
+func TestSimpleArr(t *testing.T) {
+	arr := []int{0, 11, 5, 3, 6, 7, 2, 1, 0, 99}
+	fmt.Println(quickSort(arr, 0, len(arr)-1))
 }
